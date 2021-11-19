1a00f29ec08f4731e48249e0b4743d8e0801ef7f
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2021-11-19 17:57:54 +0500

    ch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,9 @@
     }
 
 
+    .modal-backdrop.show {
+    opacity: .5;
+}
 
         .modal-backdrop {
             position: relative !important;
@@ -23,6 +26,18 @@
             height: 100vh;
             background-color: #000;
         }
+        .modal {
+    position: fixed !important;
+    top: 0;
+    left: 0;
+    z-index: 1055;
+    display: none;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    outline: 0;
+}
 
 </style>
   @section('content')
@@ -272,9 +287,11 @@ after someone embarrassed you , you might be even be thinking to take your own l
                     {{ session()->get('success') }}
                 </div>
             @endif
-
-
             <?php while ($row = $q->fetch()): ?>
+
+
+
+
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                 <div class="member">
                     <img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
@@ -288,7 +305,7 @@ after someone embarrassed you , you might be even be thinking to take your own l
                         <div class="social">
                             <!-- Button trigger modal -->
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal<?php echo htmlspecialchars($row['id']); ?>">
+                                data-bs-target="#exampleModal<?php echo htmlspecialchars($row['id']); ?>"">
                                 Ask for Appointment
                             </button>
 
@@ -305,57 +322,87 @@ after someone embarrassed you , you might be even be thinking to take your own l
 
 
 
-            <div class="modal fade" style="margin-top: 136px;" id="exampleModal<?php echo htmlspecialchars($row['id']); ?>"
-                tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content" style="margin-top: 58px;">
-                        <form action="{{ route('appointment') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="modal-header">
-
-                                <h5 class="modal-title" id="exampleModalLabel">Advisors</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-
-                            <div class="modal-body">
-
-                                <div class="container">
-                                    <select name="advis" id="" class="form-control">
-                                        <option value="">Current Advisor</option>
-                                        <option value="<?php echo htmlspecialchars($row['name']); ?>"><?php echo htmlspecialchars($row['name']); ?></option>
-
-                                    </select><br>
-                                    <label for="">Start Time</label>
-                                    <input type="time" name="start_time" class="form-control"><br>
-                                    <label for="">End Time</label>
-                                    <input type="time" name="end_time" class="form-control"><br>
-                                    <label for="">Date ofAppointment</label>
-                                    <input type="date" name="date" class="form-control"><br>
-                                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>" id="">
-                                </div>
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary"
-                                    data-bs-dismiss="modal">Cancel</button>
-                                <button type="submit" class="btn btn-primary">Set Appointments</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-
 
             <?php endwhile; ?>
 
 
+
+
+
         </div>
       </div>
+
+
     </section><!-- End Trainers Section -->
 
+
+
+
+
+
   </main><!-- End #main -->
+
+
+  <?php
+
+  try {
+      $pdo =   pdo();
+      $sql11 = 'SELECT * FROM users WHERE role = 1';
+
+      $q = $pdo->query($sql11);
+      $q->setFetchMode(PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+      die("Could not connect to the database $dbname :" . $e->getMessage());
+  }
+
+  ?>
+
+
+  <?php while ($row = $q->fetch()): ?>
+
+  <div class="modal fade" style="" id="exampleModal<?php echo htmlspecialchars($row['id']); ?>"
+    tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content" style="margin-top: 58px;">
+            <form action="{{ route('appointment') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-header">
+
+                    <h5 class="modal-title" id="exampleModalLabel">Advisors</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="container">
+                        <select name="advis" id="" class="form-control">
+                            <option value="">Current Advisor</option>
+                            <option value="<?php echo htmlspecialchars($row['name']); ?>"><?php echo htmlspecialchars($row['name']); ?></option>
+
+                        </select><br>
+                        <label for="">Start Time</label>
+                        <input type="time" name="start_time" class="form-control"><br>
+                        <label for="">End Time</label>
+                        <input type="time" name="end_time" class="form-control"><br>
+                        <label for="">Date ofAppointment</label>
+                        <input type="date" name="date" class="form-control"><br>
+                        <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>" id="">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary"
+                        data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Set Appointments</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php endwhile; ?>
+
+
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.mainlayout')
 
 @section('title')
-<title>Profile</title>
+    <title>Profile</title>
 @endsection
 
 <?php
@@ -11,7 +11,7 @@ $adv = App\Models\User::where('id', Auth::user()->id)->first();
 ?>
 
 <style>
-    .con1{
+    .con1 {
         margin-top: 150px;
         background-color: white;
         /* background: linear-gradient(to top right, #808080 15%, #d3d3d3 100%); */
@@ -19,183 +19,205 @@ $adv = App\Models\User::where('id', Auth::user()->id)->first();
         box-shadow: 0 30px 60px 0 rgb(0 0 0 / 30%);
         margin-bottom: 30px;
     }
-    .img{
+
+    .img {
         /* width: 100%; */
         max-height: 160px !important;
         border-radius: 62% !important;
-    background: white;
+        background: white;
     }
+
 </style>
 
 @section('content')
 
-<form action=" {{route('availability')}}" method="POST" enctype="multipart/form-data"  >
-    @csrf
-<div class="container con1">
-    <div class="row" style="padding:35px;">
-        <div class="col-lg-12 col-12" style="text-align: center;">
-            <img src="{{asset('images/avatar.png')}}" class=" img">
-        </div>
-        @if(session()->has('success'))
-        <div class="alert alert-success">
-            {{ session()->get('success') }}
-        </div>
-    @endif
-        <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
-            <label >Name:</label>
-            <input type="text" name="name" value="{{$adv->name}}" class="form-control" placeholder="Name">
-        </div>
-        <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
-            <label>Email:</label>
+    <form action=" {{ route('availability') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="container con1">
+            <div class="row" style="padding:35px;">
+                <div class="col-lg-12 col-12" style="text-align: center;">
+                    <img src="{{ asset('images/avatar.png') }}" class=" img">
+                </div>
+                @if (session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                @endif
+                <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
+                    <label>Name:</label>
+                    <input type="text" name="name" value="{{ $adv->name }}" class="form-control" placeholder="Name">
+                </div>
+                <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
+                    <label>Email:</label>
 
-            <input type="text"  name="email"  value="{{$adv->email}}" class="form-control" placeholder="Mobile Number">
-        </div>
-        <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
-            <label>Phone Number:</label>
+                    <input type="text" name="email" value="{{ $adv->email }}" class="form-control"
+                        placeholder="Mobile Number">
+                </div>
+                <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
+                    <label>Phone Number:</label>
 
-            <input type="text"  name="number"  value="{{$adv->number}}" class="form-control" placeholder="Phone Number">
-        </div>
-        <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
-            <label> Password:</label>
+                    <input type="text" name="number" value="{{ $adv->number }}" class="form-control"
+                        placeholder="Phone Number">
+                </div>
+                <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
+                    <label> Password:</label>
 
-            <input type="password" name="password" class="form-control" placeholder="Password">
-        </div>
-        <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
-            <label>Confirm Password:</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
+                <div class="col-lg-6 col-sm-6 col-12" style="padding:20px">
+                    <label>Confirm Password:</label>
 
-            <input type="password"  name="cpassword" class="form-control" placeholder="Confirm Password">
-        </div>
-
-
-@if(Auth::user()->role == 1)
-        <div class="col-lg-12 col-12" style="text-align: center;">
-            <h3>Availability</h3>
-        </div>
+                    <input type="password" name="cpassword" class="form-control" placeholder="Confirm Password">
+                </div>
 
 
-
-        <div class="container">
-            <div class="row">
-
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Monday</b><br>
-            <label>Start Time:</label>
-
-            <input type="time" value="{{$adv->mon_start_time}}" class="form-control" name="mon_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
-
-            <input type="time"  value="{{$adv->mon_end_time}}" class="form-control" name="mon_end_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Tuesday</b><br>
-            <label>Start Time:</label>
-
-            <input type="time"  value="{{$adv->tue_start_time}}" class="form-control" name="tue_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
-
-            <input type="time"  value="{{$adv->tue_end_time}}" class="form-control" name="tue_end_time">
-        </div>
+                @if (Auth::user()->role == 1)
+                    <div class="col-lg-12 col-12" style="text-align: center;">
+                        <h3>Availability</h3>
+                    </div>
 
 
 
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Wednesday</b><br>
-            <label>Start Time:</label>
+                    <div class="container">
+                        <div class="row">
 
-            <input type="time"  value="{{$adv->wed_start_time}}" class="form-control" name="wed_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Monday</b><br>
+                                <label>Start Time:</label>
 
-            <input type="time"  value="{{$adv->wed_end_time}}" class="form-control" name="wed_end_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Thursday</b><br>
-            <label>Start Time:</label>
+                                <input type="time" value="{{ $adv->mon_start_time }}" class="form-control"
+                                    name="mon_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
 
-            <input type="time"  value="{{$adv->thu_start_time}}" class="form-control" name="thu_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
+                                <input type="time" value="{{ $adv->mon_end_time }}" class="form-control"
+                                    name="mon_end_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Tuesday</b><br>
+                                <label>Start Time:</label>
 
-            <input type="time"  value="{{$adv->thu_end_time}}" class="form-control" name="thu_end_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Friday</b><br>
-            <label>Start Time:</label>
+                                <input type="time" value="{{ $adv->tue_start_time }}" class="form-control"
+                                    name="tue_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
 
-            <input type="time"  value="{{$adv->fri_start_time}}" class="form-control" name="fri_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
+                                <input type="time" value="{{ $adv->tue_end_time }}" class="form-control"
+                                    name="tue_end_time">
+                            </div>
 
-            <input type="time"  value="{{$adv->fri_end_time}}" class="form-control" name="fri_end_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Saturday</b><br>
-            <label>Start Time:</label>
 
-            <input type="time"  value="{{$adv->sat_start_time}}" class="form-control" name="sat_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
 
-            <input type="time"  value="{{$adv->sat_end_time}}" class="form-control" name="sat_end_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <b>Sunday</b><br>
-            <label>Start Time:</label>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Wednesday</b><br>
+                                <label>Start Time:</label>
 
-            <input type="time"  value="{{$adv->sun_start_time}}" class="form-control" name="sun_start_time">
-        </div>
-        <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
-            <br>
-            <label>End Time:</label>
+                                <input type="time" value="{{ $adv->wed_start_time }}" class="form-control"
+                                    name="wed_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
 
-            <input type="time"  value="{{$adv->sun_end_time}}" class="form-control" name="sun_end_time">
-        </div>
+                                <input type="time" value="{{ $adv->wed_end_time }}" class="form-control"
+                                    name="wed_end_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Thursday</b><br>
+                                <label>Start Time:</label>
 
+                                <input type="time" value="{{ $adv->thu_start_time }}" class="form-control"
+                                    name="thu_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
+
+                                <input type="time" value="{{ $adv->thu_end_time }}" class="form-control"
+                                    name="thu_end_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Friday</b><br>
+                                <label>Start Time:</label>
+
+                                <input type="time" value="{{ $adv->fri_start_time }}" class="form-control"
+                                    name="fri_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
+
+                                <input type="time" value="{{ $adv->fri_end_time }}" class="form-control"
+                                    name="fri_end_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Saturday</b><br>
+                                <label>Start Time:</label>
+
+                                <input type="time" value="{{ $adv->sat_start_time }}" class="form-control"
+                                    name="sat_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
+
+                                <input type="time" value="{{ $adv->sat_end_time }}" class="form-control"
+                                    name="sat_end_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <b>Sunday</b><br>
+                                <label>Start Time:</label>
+
+                                <input type="time" value="{{ $adv->sun_start_time }}" class="form-control"
+                                    name="sun_start_time">
+                            </div>
+                            <div class="col-lg-3 col-sm-6 col-12" style="padding-bottom:10px">
+                                <br>
+                                <label>End Time:</label>
+
+                                <input type="time" value="{{ $adv->sun_end_time }}" class="form-control"
+                                    name="sun_end_time">
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                @endif
+
+                <div class="container">
+
+
+
+                <div class="row">
+                    <div class="col-12" style="margin: auto;text-align: end;padding-right: 20px;">
+                        <button type="submit" class="btn btn-primary">Submit </button>
+                    </div>
+
+                </div>
+            </div>
+
+    </form>
+    </div>
     </div>
 
 
-</div>
-@endif
-
-<div class="row">
-    <div class="col-12" style="margin: auto;text-align: end;">
-        <button type="submit" class="btn btn-primary">Submit </button>
-    </div>
-
-</div>
 
 
-</form>
-    </div>
-</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
+    <script>
+        $(document).ready(function() {
+
+            $('.nav-item').removeClass("active1");
+            $('#Profie').addClass("active1");
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-
-<script>
-  $(document).ready(function () {
-
-      $('.nav-item').removeClass("active1");
-  $('#Profie').addClass("active1");
-
-
-  });
-</script>
+        });
+    </script>
 @endsection
