0d762e04a1b58e41ef7d2b4a1802622ddd5aa6ed
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2021-11-19 19:09:08 +0500

    ch

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -66,14 +66,14 @@
           <li  class="nav-item"><a href="{{url('/contact')}}" id="Contact" class="nav-link">Contact us</a></li>
           <li  class="nav-item"><a href="{{url('/privacy')}}" id="Rules" class="nav-link">Rules</a></li>
 
-          <li  class="nav-item"><a href="{{url('/chat')}}" id="chat" class="nav-link">Chat</a></li>
-
 
           @if (Auth::check())
           @if (Auth::user()->role == 1)
           <li  class="nav-item"><a  href="{{url('/advisor_booking')}}" class="nav-link" id="Advisor">Advisor Booking</a></li>
           @else
           <li  class="nav-item"><a href="{{url('/booking')}}" class="nav-link" id="Booking">Booking</a></li>
+          <li  class="nav-item"><a href="{{url('/chat')}}" id="chat" class="nav-link">Chat</a></li>
+
 
           @endif
           <li  class="nav-item"><a href="{{url('/profile')}}" id="Profie" class="nav-link">Profile</a></li>
