e3d91c160d990ea75601cc46e5043ee887907cef
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2021-11-20 10:33:31 +0500

    c

--- resources/views/advisors.blade.php
+++ resources/views/advisors.blade.php
@@ -57,22 +57,29 @@
                         </div>
                     @endif
 
-
                     <?php while ($row = $q->fetch()): ?>
+
+
+
+
                     <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                         <div class="member">
-                            <img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
+
+                            {{-- <img src="{{asset($row['img'])}}" class="img-fluid" alt=""> --}}
+                            @if(isset($row['img']))
+                            <img src="images/<?php echo $row['img']?>" style="max-height: 58%; height: 58%;width: 100%;max-width: 100%;min-width: 100%;min-height: 58%;">
+                            @else
+                            <img src="images/avatar.png" style="max-height: 58%; height: 58%;width: 100%;max-width: 100%;min-width: 100%;min-height: 58%;">
+
+                            @endif
                             <div class="member-content">
                                 <h4><?php echo htmlspecialchars($row['name']); ?></h4>
                                 <span><?php echo htmlspecialchars($row['email']); ?></span>
-                                <p>
-                                    Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis
-                                    quaerat qui aut aut aut
-                                </p>
+
                                 <div class="social">
                                     <!-- Button trigger modal -->
                                     <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                        data-bs-target="#exampleModal<?php echo htmlspecialchars($row['id']); ?>">
+                                        data-bs-target="#exampleModal<?php echo htmlspecialchars($row['id']); ?>"">
                                         Ask for Appointment
                                     </button>
 
@@ -89,50 +96,9 @@
 
 
 
-                    <div class="modal fade" style="margin-top: 136px;" id="exampleModal<?php echo htmlspecialchars($row['id']); ?>"
-                        tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content" style="margin-top: 58px;">
-                                <form action="{{ route('appointment') }}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="modal-header">
-
-                                        <h5 class="modal-title" id="exampleModalLabel">Advisors</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-
-                                    <div class="modal-body">
-
-                                        <div class="container">
-                                            <select name="advis" id="" class="form-control">
-                                                <option value="">Current Advisor</option>
-                                                <option value="<?php echo htmlspecialchars($row['name']); ?>"><?php echo htmlspecialchars($row['name']); ?></option>
-
-                                            </select><br>
-                                            <label for="">Start Time</label>
-                                            <input type="time" name="start_time" class="form-control"><br>
-                                            <label for="">End Time</label>
-                                            <input type="time" name="end_time" class="form-control"><br>
-                                            <label for="">Date ofAppointment</label>
-                                            <input type="date" name="date" class="form-control"><br>
-                                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>" id="">
-                                        </div>
-                                    </div>
-
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary"
-                                            data-bs-dismiss="modal">Cancel</button>
-                                        <button type="submit" class="btn btn-primary">Set Appointments</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-
 
                     <?php endwhile; ?>
+
 
 
                 </div>
@@ -142,6 +108,64 @@
         </section><!-- End Trainers Section -->
 
     </main><!-- End #main -->
+    <?php
+
+    try {
+        $pdo =   pdo();
+        $sql11 = 'SELECT * FROM users WHERE role = 1';
+
+        $q = $pdo->query($sql11);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Could not connect to the database $dbname :" . $e->getMessage());
+    }
+
+    ?>
+
+
+    <?php while ($row = $q->fetch()): ?>
+
+    <div class="modal fade" style="" id="exampleModal<?php echo htmlspecialchars($row['id']); ?>"
+      tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content" style="margin-top: 58px;">
+              <form action="{{ route('appointment') }}" method="POST" enctype="multipart/form-data">
+                  @csrf
+                  <div class="modal-header">
+
+                      <h5 class="modal-title" id="exampleModalLabel">Advisors</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal"
+                          aria-label="Close"></button>
+                  </div>
+
+                  <div class="modal-body">
+                      <div class="container">
+                          <select name="advis" id="" class="form-control">
+                              <option value="">Current Advisor</option>
+                              <option value="<?php echo htmlspecialchars($row['name']); ?>"><?php echo htmlspecialchars($row['name']); ?></option>
+
+                          </select><br>
+                          <label for="">Start Time</label>
+                          <input type="time" name="start_time" class="form-control"><br>
+                          <label for="">End Time</label>
+                          <input type="time" name="end_time" class="form-control"><br>
+                          <label for="">Date ofAppointment</label>
+                          <input type="date" name="date" class="form-control"><br>
+                          <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>" id="">
+                      </div>
+                  </div>
+
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary"
+                          data-bs-dismiss="modal">Cancel</button>
+                      <button type="submit" class="btn btn-primary">Set Appointments</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+
+  <?php endwhile; ?>
 
 
 
